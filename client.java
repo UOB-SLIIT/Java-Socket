6121aca586d0551150de2b91a46b5701d9dc2ae8
Salitha Kaweendra <salithak1@gmail.com>
2019-12-07 12:07:56 +0530

    Add files via upload

---
+++ client.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class client{
+    public static void main(String[] args)throws IOException{
+        Socket s = new Socket("localhost",4999);
+
+    }
+}
