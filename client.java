a8a2e5261d28b0b66bd0dec8be07b4fc8f9ee157
Salitha Kaweendra <salithak1@gmail.com>
2019-12-16 20:29:59 +0530

    documentation
    adding comments for better understanding

--- client.java
+++ client.java
@@ -3,7 +3,7 @@ import java.net.Socket;
 
 public class client{
     public static void main(String[] args)throws IOException{
-        Socket s = new Socket("localhost",4999);
+        Socket s = new Socket("localhost",4999); //port(4999) replace the "localhost" with the server ip if you are using 2 pcs
 
     }
 }
